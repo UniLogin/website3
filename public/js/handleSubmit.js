49cda34103939b5458fcb4612620c5006e17be3c
Albert Islamov <higashi166@gmail.com>
2020-02-14 16:02:51 +0100

    Add subscribe styles

--- public/js/handleSubmit.js
+++ public/js/handleSubmit.js
@@ -1,6 +1,9 @@
 function handleSubmit(event) {
   event.preventDefault();
   const title = document.getElementById('signup-form__title');
+  const input = document.querySelector('.signup-form__input');
+  const button = document.querySelector('.signup-form__button');
+  const form = document.getElementById('signup-form');
   if (!event.target.email.value) {
       title.innerText = 'Email cannot be empty. Try again!'
       return false;
@@ -14,7 +17,16 @@ function handleSubmit(event) {
       method: event.target.method,
       body: JSON.stringify({email: event.target.email.value})
   }).then(
-      () => {title.innerHTML = "<b>Thank you</b> for your subscription!"},
+      () => {
+        const subtitle = document.createElement('P');
+        subtitle.innerText = 'You have been successfully subscribed to our pilot program.';
+        subtitle.classList.add('signup-form__subtitle');
+        title.style.color = '#B55EF7';
+        title.innerHTML = '<b>Congrats!</b>';
+        button.style.display = 'none';
+        input.style.display = 'none';
+        form.appendChild(subtitle);
+      },
       () => {title.innerHTML = '<b>Something went wrong.</b> Please, try again.'}
   )
 }
