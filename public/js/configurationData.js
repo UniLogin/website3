1271dee70a3254b79ad62133e14762ccbbd896e1
Albert Islamov <higashi166@gmail.com>
2020-02-26 16:58:51 +0100

    Introduce new pickers

---
+++ public/js/configurationData.js
@@ -0,0 +1,93 @@
+export const ts_ethers_metamask_unilogin = `
+import ULIFrameProvider from '@unilogin/provider'
+import { providers, utils } from 'ethers'
+
+const provider = new providers.Web3Provider(ULIFrameProvider.createPicker(window.ethereum))
+const signer = provider.getSigner()
+const transaction = await signer.sendTransaction({
+  to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+  value: utils.parseEther('0.003'),
+})
+`
+export const ts_web3_metamask_unilogin = `
+import ULIFrameProvider from '@unilogin/provider'
+import Web3 from 'web3'
+
+const web3 = new Web3(ULIFrameProvider.createPicker(window.ethereum))
+const [account] = await web3.eth.getAccounts()
+const tx = await web3.eth.sendTransaction({
+  from: account,
+  to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+  value: web3.utils.toWei('0.003'),
+})
+`
+export const ts_ethers_unilogin = `
+import UniLogin from ‘@unilogin/provider’
+import {providers, utils} from ‘ethers’
+
+const provider = new providers.Web3Provider(UniLogin.create(‘mainnet’))
+const signer = provider.getSigner()
+const transaction = await signer.sendTransaction({
+  to: ‘0xF80fd6F5eF91230805508bB28d75248024E50F6F’,
+  value: utils.parseEther(‘0.003’),
+})
+`
+
+export const ts_web3_unilogin = `
+import UniLogin from '@unilogin/provider'
+import web3 from 'web3'
+const web3 = new Web3()
+const [account] = await web3.eth.getAccounts(UniLogin.create('mainnet'))
+const tx = await web3.eth.sendTransaction({
+  from: account,
+  to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+  value: web3.utils.toWei('0.003'),
+})
+`
+
+export const js_ethers_metamask_unilogin = `
+const ULIFrameProvider = require('@unilogin/provider')
+const { providers, utils } = require('ethers')
+const provider = new providers.Web3Provider(ULIFrameProvider.createPicker(window.ethereum))
+const signer = provider.getSigner()
+signer.sendTransaction({
+  to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+  value: utils.parseEther('0.003'),
+}).then(console.log)
+`
+
+export const js_web3_metamask_unilogin = `
+const ULIFrameProvider = require('@unilogin/provider')
+const Web3 = require('web3')
+const web3 = new Web3(ULIFrameProvider.createPicker(window.ethereum))
+web3.eth.getAccounts()
+  .then(accounts => web3.eth.sendTransaction({
+    from: accounts[0],
+    to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+    value: web3.utils.toWei('0.003'),
+  }).then(console.log))
+`
+
+export const js_ethers_unilogin = `
+const UniLogin = require('@unilogin/provider')
+const {providers, utils} = require('ethers')
+const provider = new providers.Web3Provider(UniLogin.create('mainnet'))
+const signer = provider.getSigner()
+signer.sendTransaction({
+  to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+  value: utils.parseEther('0.003'),
+}).then(console.log)
+`
+
+export const js_web3_unilogin = `
+const UniLogin = require('@unilogin/provider')
+const web3 = require('web3')
+const web3 = new Web3(UniLogin.create('mainnet'))
+web3.eth.getAccounts()
+  .then(accounts => web3.eth.sendTransaction({
+    from: accounts[0],
+    to: '0xF80fd6F5eF91230805508bB28d75248024E50F6F',
+    value: web3.utils.toWei('0.003'),
+  }).then(console.log))
+`
+
---
+++ public/js/pickerHandler.js
@@ -0,0 +1,83 @@
+import Prism from 'prismjs';
+import { ts_ethers_metamask_unilogin, ts_web3_metamask_unilogin, ts_ethers_unilogin, ts_web3_unilogin } from "./configurationData";
+import { js_ethers_metamask_unilogin, js_web3_metamask_unilogin, js_ethers_unilogin, js_web3_unilogin } from "./configurationData";
+
+export const optionHandler = (options, callback) => {
+    let selectedValue = "";
+    for (const option of options) {
+        option.addEventListener('click', () => {
+            removeSelection(options);
+            option.classList.add('active');
+            selectedValue = option.getAttribute('data-value');
+        });
+    };
+} 
+
+export const removeSelection = (options) => {
+    for (const option of options) {
+        option.classList.remove('active');
+    }
+}
+
+export const getConfiguration = (optionValues) => {
+    console.log(optionValues.join(','));
+    switch (optionValues.join(',')) {
+        case 'TypeScript,Ether.js,Metamask/UniLogin': return ts_ethers_metamask_unilogin;
+        case 'TypeScript,web3.js,Metamask/UniLogin': return ts_web3_metamask_unilogin;
+        case 'TypeScript,Ether.js,UniLogin': return ts_ethers_unilogin;
+        case 'TypeScript,web3.js,UniLogin': return ts_web3_unilogin;
+        case 'JavaScript,Ether.js,Metamask/UniLogin': return js_ethers_metamask_unilogin;
+        case 'JavaScript,web3.js,Metamask/UniLogin': return js_web3_metamask_unilogin;
+        case 'JavaScript,Ether.js,UniLogin': return js_ethers_unilogin;
+        case 'JavaScript,web3.js,UniLogin': return js_web3_unilogin;
+        default: console.log('none');
+    }
+}
+
+const getInitialValues = (wrapper) => {
+    return wrapper.querySelector('.active').getAttribute('data-value');
+}
+
+export const configurationPicker = (languageOptions, libraryOptions, providerOptions, output) => {
+    let language = getInitialValues(languageOptions[0].parentElement);
+    let library = getInitialValues(libraryOptions[0].parentElement);
+    let provider = getInitialValues(providerOptions[0].parentElement); 
+    output.innerHTML = Prism.highlight(getConfiguration([language, library, provider]), Prism.languages.javascript, 'javascript');
+
+    for (const item of languageOptions) {
+        item.addEventListener('click', () => {
+            removeSelection(languageOptions);
+            item.classList.add('active');
+            language = item.getAttribute('data-value');
+            output.innerHTML = Prism.highlight(getConfiguration([language, library, provider]), Prism.languages.javascript, 'javascript'); 
+        });
+    };
+
+    for (const item of libraryOptions) {
+        item.addEventListener('click', () => {
+            removeSelection(libraryOptions);
+            item.classList.add('active');
+            library = item.getAttribute('data-value');
+            output.innerHTML = Prism.highlight(getConfiguration([language, library, provider]), Prism.languages.javascript, 'javascript'); 
+        });
+    };
+
+    for (const item of providerOptions) {
+        item.addEventListener('click', () => {
+            removeSelection(providerOptions);
+            item.classList.add('active');
+            provider = item.getAttribute('data-value');
+            output.innerHTML = Prism.highlight(getConfiguration([language, library, provider]), Prism.languages.javascript, 'javascript'); 
+        });
+    };
+}
+
+export const managerHandler = (options, output) => {
+    for (const option of options) {
+        option.addEventListener('click', () => {
+            removeSelection(options);
+            option.classList.add('active');
+            output.innerHTML = option.getAttribute('data-value');
+        });
+    };
+};
